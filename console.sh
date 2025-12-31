7d965eb95139a7971bc25f25717efe3c1b9e995f
valentinritz-coder <valentin.ritz@icloud.com>
2025-12-31 12:31:54 +0100

    Update console.sh

--- console.sh
+++ console.sh
@@ -1,7 +1,4 @@
-su -c 'mkdir -p /data/adb/service.d'
-su -c 'cat > /data/adb/service.d/90-adbd-tcp.sh << "EOF"
-#!/system/bin/sh
-setprop service.adb.tcp.port 5555
-setprop ctl.restart adbd
-EOF'
-su -c 'chmod 755 /data/adb/service.d/90-adbd-tcp.sh'
+adb kill-server 2>/dev/null || true
+pkill -f adb 2>/dev/null || true
+adb start-server
+adb devices -l
