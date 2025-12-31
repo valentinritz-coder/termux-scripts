4c1dea3ebe13d5602dfae6351f1c8bfaf24ffa75
valentinritz-coder <valentin.ritz@icloud.com>
2025-12-31 12:11:35 +0100

    Update console.sh

--- console.sh
+++ console.sh
@@ -1,2 +1,7 @@
-su -c 'getprop service.adb.tcp.port; getprop init.svc.adbd'
-su -c 'ss -ltn 2>/dev/null | grep -E ":(5555)\b" || netstat -ltn 2>/dev/null | grep -E ":(5555)\b" || echo "NO_LISTENER_5555"'
+su -c 'mkdir -p /data/adb/service.d'
+su -c 'cat > /data/adb/service.d/90-adbd-tcp.sh << "EOF"
+#!/system/bin/sh
+setprop service.adb.tcp.port 5555
+setprop ctl.restart adbd
+EOF'
+su -c 'chmod 755 /data/adb/service.d/90-adbd-tcp.sh'
