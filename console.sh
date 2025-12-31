05a64f3caf993a7506dd49eaee380eb5d4e0197d
valentinritz-coder <valentin.ritz@icloud.com>
2025-12-31 12:05:33 +0100

    Update console.sh

--- console.sh
+++ console.sh
@@ -1,2 +1,2 @@
-bash /sdcard/cfl_watch/adb_local.sh start
-adb devices -l
+su -c 'getprop service.adb.tcp.port; getprop init.svc.adbd'
+su -c 'ss -ltn 2>/dev/null | grep -E ":(5555)\b" || netstat -ltn 2>/dev/null | grep -E ":(5555)\b" || echo "NO_LISTENER_5555"'
