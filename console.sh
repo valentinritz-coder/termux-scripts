d79635d7e4d79918139be2ddfae2eae4a6441210
valentinritz-coder <valentin.ritz@icloud.com>
2025-12-31 13:08:39 +0100

    Update console.sh

--- console.sh
+++ console.sh
@@ -1,1 +1,5 @@
-su -c 'runcon u:r:shell:s0 /system/bin/input tap 100 100; echo "rc=$?"'
+ls -l /system/bin/input || true
+
+su -c 'ls -l /system/bin/input; /system/bin/input tap 100 100; echo "rc=$?"' || true
+
+su -c 'runcon u:r:shell:s0 /system/bin/input tap 100 100; echo "rc=$?"' || true
