e6e2bc16ad60e6e7a4032c8fa2d60b3de1a0d2ec
valentinritz-coder <valentin.ritz@icloud.com>
2025-12-31 12:36:20 +0100

    Update console.sh

--- console.sh
+++ console.sh
@@ -1,4 +1,8 @@
-adb kill-server 2>/dev/null || true
-pkill -f adb 2>/dev/null || true
+SER="192.168.118.150:39973"
+
+adb kill-server >/dev/null 2>&1 || true
 adb start-server
+
+adb connect "$SER"
 adb devices -l
+adb -s "$SER" shell id -Z
