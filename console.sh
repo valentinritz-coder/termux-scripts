f2a7c54a00c690ec31f638e09b14f23e427b428a
valentinritz-coder <valentin.ritz@icloud.com>
2026-01-02 14:27:44 +0100

    Delete console.sh

--- console.sh
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-set -euo pipefail
-
-CFL_CODE_DIR="${CFL_CODE_DIR:-~/cfl_watch}"
-ADB_TCP_PORT="${ADB_TCP_PORT:-37099}" bash "$CFL_CODE_DIR/runner.sh" "$@"
